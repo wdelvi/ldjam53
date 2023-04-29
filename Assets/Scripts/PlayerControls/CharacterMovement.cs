211f8fe7c4c938000507a8669a6e3ed88d806f54
TooLoo <17146137+T00L00@users.noreply.github.com>
2023-04-28 23:39:03 -0500

    Simple 2.5D camera and player controls

---
+++ Assets/Scripts/PlayerControls/CharacterMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YATE
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField] private CharacterController characterController;
+
+        [Header("Movement Settings")]
+        [SerializeField] private float moveSpeed = 10f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (characterController != null)
+            {
+                HandleMovement();
+            }
+        }
+
+        private void HandleMovement()
+        {
+            Vector3 inputDir = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W)) inputDir.z = 1f;
+            if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
+            if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
+            if (Input.GetKey(KeyCode.D)) inputDir.x = 1f;
+
+            Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+            characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+        }
+    }
+}
